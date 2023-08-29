32d674fb01418a153888de46f7de10d9f2023b63
GuizinMEndes2 <126169397+GuizinMEndes2@users.noreply.github.com>
2023-08-29 17:28:45 -0300

    add

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('login',[AuthController::class, 'login']);
+Route::post('register',[AuthController::class, 'register']);
+Route::post('logout',[AuthController::class, 'logout']);
+Route::post('refresh',[AuthController::class, 'refresh']);
+Route::get('test', [AuthController::class, 'test']);
